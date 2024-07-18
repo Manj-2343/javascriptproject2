a466c242c666db4ca79508804d3a880bf5d84495
Manoj Biswal <manojbiswal521@gmail.com>
2024-07-18 13:40:07 +0530

    new add

--- 20.js
+++ 20.js
@@ -1,4 +1,17 @@
 //array destructing
-const myArray = ["value1", "value2"];
-let [myArray1, myArray2] = myArray;
-console.log("my 1st value is ", myArray1, "and my 2nd value is", myArray2);
+const myArray = ["value1", "value2", "value3", "value4", "value5"];
+// let [myArray1, myArray2] = myArray;
+// console.log("my 1st value is", myArray1, "and my 2nd value is", myArray2);
+// how to get value3 in myArray2;
+// let [myArray3, , myArray4] = myArray;
+// console.log("my 1st value is", myArray3, "and my 2nd value is", myArray4);
+// how to get the value 4 and 5 simultaneously
+let [myArray1, myArray2, ...myArray3] = myArray;
+console.log(
+  "my 1st value is",
+  myArray1,
+  "and my 2nd value is",
+  myArray2,
+  "and rest value in",
+  myArray3
+);
