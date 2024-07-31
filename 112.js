5fe356bd0eab07760c865b7e173aee2b5cf790a7
Manoj Biswal <manojbiswal521@gmail.com>
2024-08-01 03:38:00 +0530

    add new

--- 112.js
+++ 112.js
@@ -1,1 +1,15 @@
 //event object
+//by using general function
+/**const firstButton = document.querySelector("#one");
+
+firstButton.addEventListener("click", function () {
+  console.log(this.textContent); //
+});*/
+
+//by Using Arrow function
+const firstButton = document.querySelector("#one");
+
+firstButton.addEventListener("click", (e) => {
+  //   console.log(e.target.textContent);
+  console.log(e.currentTarget.textContent);
+});
